fb2931dd855e96d7ef01ccc09b0836b474d19351
FADLYxSIUU <156740583+FADLYxSIUU@users.noreply.github.com>
2024-01-30 14:52:36 +0700

    Add files via upload

---
+++ mainsetsiu.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.highmain;
+
+/**
+ *
+ * @author EBOOK123
+ */
+public class mainsetsiu {
+    public static void main(String[] args) {
+         Highmain dian = new Highmain();
+
+// menggunakan method setter
+dian.setUsername("dian");
+dian.setPassword("kopiJava");
+//
+// menggunakan method getter
+System.out.println("Username: " + dian.getUsername());
+System.out.println("Password: " + dian.getPassword());  
+    }
+}
